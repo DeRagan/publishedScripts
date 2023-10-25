3f9036993ab8ebc2bb6399b934f208ec2e9ae5fc
Andrew Oliver <112861474+DeRagan@users.noreply.github.com>
2023-10-25 09:35:39 +0100

    Create disable_ssh_password_auth.sh

---
+++ disable_ssh_password_auth.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# SSH Security Configuration Script
+# This script enhances the security of your SSH server by configuring important settings.
+# Before running this script, make sure you have added your SSH keys to your user account for secure authentication.
+
+# Check if the user is root; the script should be run with sudo.
+if [[ $EUID -ne 0 ]]; then
+  echo "Please run this script with superuser privileges (sudo)."
+  exit 1
+fi
+
+# Reminder for the user to add their SSH keys
+
+echo "Before continuing, ensure that you have added your SSH keys for secure authentication."
+
+read -p "Have you added your SSH keys? (y/n): " keys_added
+
+if [ "$keys_added" != "y" ]; then
+  echo "Please add your SSH keys before running this script."
+  exit 1
+fi
+
+# Add an include directive to load additional SSH configuration files
+echo "Include /etc/ssh/sshd_config.d/*.conf" | sudo tee -a /etc/ssh/sshd_config
+
+# Configure SSH settings to enhance security by disabling root login
+echo "ChallengeResponseAuthentication no" | sudo tee -a /etc/ssh/sshd_config.d/disable_root_login.conf
+echo "PasswordAuthentication no" | sudo tee -a /etc/ssh/sshd_config.d/disable_root_login.conf
+echo "UsePAM no" | sudo tee -a /etc/ssh/sshd_config.d/disable_root_login.conf
+echo "PermitRootLogin no" | sudo tee -a /etc/ssh/sshd_config.d/disable_root_login.conf
+
+# Restart the SSH service to apply the changes
+sudo systemctl restart ssh
+
+echo "SSH security configuration applied. Root login is disabled, password authentication is disabled, and additional settings are included in separate files."
